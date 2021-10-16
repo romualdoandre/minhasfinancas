082e909fa572a89f6fb77b8666292eabd6029aaa
Romualdo <romualdoandre@gmail.com>
2021-10-16 15:28:25 -0300

    Uso de lombok nas entidades Criação de repositories

---
+++ src/main/java/com/romualdoandre/minhasfinancas/model/repository/LancamentoRepository.java
@@ -0,0 +1,24 @@
+package com.romualdoandre.minhasfinancas.model.repository;
+
+import java.math.BigDecimal;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import com.romualdoandre.minhasfinancas.model.entity.Lancamento;
+import com.romualdoandre.minhasfinancas.model.enums.StatusLancamento;
+import com.romualdoandre.minhasfinancas.model.enums.TipoLancamento;
+
+
+public interface LancamentoRepository extends JpaRepository<Lancamento, Long> {
+
+	@Query( value = 
+			  " select sum(l.valor) from Lancamento l join l.usuario u "
+			+ " where u.id = :idUsuario and l.tipo =:tipo and l.status = :status group by u " )
+	BigDecimal obterSaldoPorTipoLancamentoEUsuarioEStatus(
+			@Param("idUsuario") Long idUsuario, 
+			@Param("tipo") TipoLancamento tipo,
+			@Param("status") StatusLancamento status);
+	
+}
