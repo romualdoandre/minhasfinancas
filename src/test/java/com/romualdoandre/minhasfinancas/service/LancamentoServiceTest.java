a4e891581f72781541dad35a78cfd878c3155a99
romualdo.andre <romualdo.andre@gmail.com>
2023-10-16 11:12:54 -0300

    Teste de LancamentoService

---
+++ src/test/java/com/romualdoandre/minhasfinancas/service/LancamentoServiceTest.java
@@ -0,0 +1,46 @@
+package com.romualdoandre.minhasfinancas.service;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mockito;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.boot.test.mock.mockito.SpyBean;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import com.romualdoandre.minhasfinancas.model.entity.Lancamento;
+import com.romualdoandre.minhasfinancas.model.enums.StatusLancamento;
+import com.romualdoandre.minhasfinancas.model.repository.LancamentoRepository;
+import com.romualdoandre.minhasfinancas.model.repository.LancamentoRepositoryTest;
+import com.romualdoandre.minhasfinancas.service.impl.LancamentoServiceImpl;
+
+@ActiveProfiles("test")
+@ExtendWith(SpringExtension.class)
+class LancamentoServiceTest {
+
+	@SpyBean
+	LancamentoServiceImpl service;
+	
+	@MockBean
+	LancamentoRepository repository;
+	@Test
+	public void deveSalvarUmLancamento() {
+		Lancamento lancamentoASalvar = LancamentoRepositoryTest.criarLancamento();
+		Lancamento lancamentoSalvo = LancamentoRepositoryTest.criarLancamento();
+		lancamentoSalvo.setId(1l);
+		Mockito.doNothing().when(service).validar(lancamentoASalvar);
+		Mockito.when(repository.save(lancamentoASalvar)).thenReturn(lancamentoSalvo);
+		
+		Lancamento lancamento = service.salvar(lancamentoASalvar);
+		assertThat(lancamento.getId()).isEqualTo(lancamentoSalvo.getId());
+		assertThat(lancamento.getStatus()).isEqualTo(StatusLancamento.PENDENTE);
+	}
+	
+	@Test
+	public void naoDeveSalvarUmlancamentoQuandoHouverErroDeValidacao() {
+		
+	}
+
+}
